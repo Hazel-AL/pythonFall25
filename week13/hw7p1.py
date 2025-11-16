788d8f1962a6045fb44c06bc12846e2950eeb6fd
Hazel Lindsey <clindsey7266@floridapoly.edu>
2025-11-16 03:51:56 +0000

    commented hw files

--- week13/hw7p1.py
+++ week13/hw7p1.py
@@ -3,8 +3,8 @@ from nicegui import ui
 
 ui.label("Simple hashing algorithm for HW7.").classes("text-lg")
 
-def twistHash():
-    inp = inputField.value
+# algorithm for hashing
+def twistHash(inp: str) -> int:
     h = 0x9E3779B1
     for c in inp:
         h = h ^ ord(c)
@@ -13,12 +13,13 @@ def twistHash():
     h = h ^ len(inp)
     return int(h)
 
-def displayHash():
-    hashLabel.text = f"Hash value: {twistHash()}"
+# displays hash, runs on a click of the button
+def displayHash() -> None:
+    hashLabel.text = f"Hash value: {twistHash(inputField.value)}"
 
+# create the card that holds all info for hashing
 with ui.card().classes("w-66"):
     inputField = ui.input("Enter text to hash:")
     ui.button("GET HASH", color="green", on_click=displayHash)
     hashLabel = ui.label("Hash Value: ")
-
 ui.run(title="Example of twistHash algorithm.")
--- week13/hw7p2.py
+++ week13/hw7p2.py
@@ -9,33 +9,45 @@ opened = [] # indices of currently flipped cards
 matched = [] # indices of solved cards
 # TODO 2: Write function to flip non-matching cards back
 def reset_pair(i, j):
+    # function necessary to run with ui.timer()
     def flip_back():
+        # i and j are indeces, if they arent matched reset
         for idx in (i, j):
             if idx not in matched:
                 buttons[idx].text = "❓"
+        # and clear opened[]
         opened.clear()
+    # delay for 0.5 sec, then run the actual function
     ui.timer(0.5, lambda: flip_back(), once=True)
 # TODO 3: Write click handler
+# takes the index of the card clicked 
 def handle_click(idx: int) -> None:
+    # exit function if the button is already opened
     if idx in opened or idx in matched:
         return
+    # flip over the button, set the text to the matching emoji
     buttons[idx].text = emojis[idx]
+    # add the button to opened[]
     opened.append(idx)
+    # if 2 buttons are flipped:
     if len(opened) >= 2:
+        # i, j makes code easier to read
         i, j = opened
+        # if the same, add both to matched[] and clear opened[]
         if emojis[i] == emojis[j]:
             matched.append(i)
             matched.append(j)
             opened.clear()
-            
+            # if all are matched, notify user
             if len(matched) == 16:
                 ui.notify("You win!", position="top")
+        # otherwise reset
         else:
             reset_pair(i, j)
 # Build 4x4 grid
 with ui.grid(columns=4):
-# TODO 4: Create 16 buttons
+    # creates 16 buttons for the game
     for i in range(0,16):
-        item = ui.button("❓", on_click=lambda i=i: handle_click(i)).classes("w-40 h-40 text-lg")
+        item = ui.button("❓", on_click=lambda i=i: handle_click(i)).classes("w-40 h-50 text-lg")
         buttons.append(item)
 ui.run(title='Memory Game')
